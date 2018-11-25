99069ffc850493e102bb5be98cd9ffff4409656b
davidferro <david-ferro@sbcglobal.net>
2018-11-25 17:11:09 -0500

    finished questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,9 +16,9 @@ var questionCounter = 0;
 var optRand = -1;
 var questionArr = [];
 var question1 = {
-    question: "In what year did the Disney cartoon \"Steamboat Willie\" premiere?",
+    question: "In what year did the Walt Disney cartoon \"Steamboat Willie\" premiere?",
     correct: "1928",
-    incorrect: ["1926", "1930", "1942"]
+    incorrect: ["1926", "1930", "1924"]
 }
 var question2 = {
     question: "Which fictional television character from the late 1980s had the catchphrase \"I kill me!\"?",
@@ -26,44 +26,44 @@ var question2 = {
     incorrect: ["Cliff Clavin", "Rose Nylund", "Alex Keaton"]
 }
 var question3 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "Iowa borders both Illinois and Nebraska.  What other state borders both Illinois and Nebraska?",
+    correct: "Missouri",
+    incorrect: ["Arkansas", "Kansas", "Minnesota"]
 }
 var question4 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "Who won a Nobel Peace Prize in 1906 for his work at ending the Russo-Japanese War?",
+    correct: "Theodore Roosevelt",
+    incorrect: ["Tsar Nicolas II", "Emperor Wilhelm II", "Mahatma Gandhi"]
 }
 var question5 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "Whose paintings include \"The Red Room\", \"The Green Stripe\", \"The Blue Nudes\", and \"The Yellow Odalisque\"?",
+    correct: "Henri Matisse",
+    incorrect: ["Pablo Picasso", "Edgar Degas", "Vincent van Gogh"]
 }
 var question6 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "What did the \"Manhattan\" project set out to develop?",
+    correct: "The Atomic Bomb",
+    incorrect: ["Central Park", "The Empire State Building", "The National Aeronautics and Space Administration"]
 }
 var question7 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "In what year was the wreck of the Titanic discovered?",
+    correct: "1985",
+    incorrect: ["1912", "1976", "1980"]
 }
 var question8 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "What nation removed the prefix \"Trans\" from before its name in June 1949?",
+    correct: "Transjordan",
+    incorrect: ["Transylvania", "Transnistria", "Transsiberia"]
 }
 var question9 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "How much does it cost to buy a utility in a standard game of Monopoly?",
+    correct: "$150",
+    incorrect: ["$75", "$200", "$175"]
 }
 var question10 = {
-    question: "",
-    correct: "",
-    incorrect: ["", "", ""]
+    question: "In Walt Disney's 1951 film \"Alice in Wonderland\", who is the companion of The Walrus?",
+    correct: "The Carpenter",
+    incorrect: ["The Mad Hatter", "The March Hare", "The Eggman"]
 }
 
 questionArr[0] = question1;
@@ -106,3 +106,4 @@ $("#startButton").on("click", function() {
     newQuestion(questionCounter);
     questionCounter++;
 });
+
